49d3181667dc5aad8f71bce8ba2bbe4fb7430848
Luis Manuel Torres Trevino <b4ar.soft@gmail.com>
2019-05-09 13:12:54 -0500

    add the server files

--- src/index.js
+++ src/index.js
@@ -1,2 +1,12 @@
-const test = 'test'
-console.log('Works');
+import '@babel/polyfill';
+
+import server from './server'
+
+
+
+async function main() {
+    await server.listen('3000');
+    console.log('Server on port 3000');
+}
+
+main();
