6ab849fc2026f90b3e93a87bfa5799f769763f70
Luis Manuel Torres Trevino <b4ar.soft@gmail.com>
2019-05-09 13:23:12 -0500

    Add middlewares in the server

--- src/server.js
+++ src/server.js
@@ -1,7 +1,18 @@
-import express from 'express';
+import express, { urlencoded, json } from 'express';
 
 const app = express();
 
+// Importing Routes
+
 app.get('/', (req, res) => res.send('Hello world'));
+
+//settings
+app.set('port', process.env.PORT || 4000);
+
+// Middlewares
+app.use(urlencoded({ extended: false }));
+app.use(json());
+
+//routes
 
 export default app;
